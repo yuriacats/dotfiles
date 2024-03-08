c2e7b16d657a6d39cb7c13e3eee7270ba23f793d
HimekaTsuchida <yuria.soft0615@gmail.com>
2024-03-08 18:10:37 +0900

    change dir

--- .config/nvim/init.lua
+++
@@ -1,8 +0,0 @@
-require("config.base")
-require("config.options")
-
-if vim.g.vscode then
-  require("config.vscode")
-else
-  require("config.onlynvim")
-end
--- .config/nvim/lua/config/onlynvim.lua
+++
@@ -1,5 +0,0 @@
-print("welcome to onlynvim")
-
-vim.api.nvim_create_user_command("Memo", function(opts)
-  vim.cmd("e " .. "~/_/memo/memo.md")
-end, {})
